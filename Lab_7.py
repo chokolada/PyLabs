69a08000a2c3e717168c2c5a0edc0450c8d71268
Maks Bornak <103888612+chokolada@users.noreply.github.com>
2023-05-14 14:49:35 +0300

    init commit

---
+++ Lab_7.py
@@ -0,0 +1,35 @@
+class Plate:
+    instance = None  # static field
+
+    def __str__(self):
+        return f"Plate(diameter={self.diameter}, material='{self.material}', color='{self.color}', isClean={self.isClean}, hasFood={self.hasFood})"
+
+    def __init__(self, diameter, material, color, isClean=False, hasFood=False):
+        self.diameter = diameter
+        self.material = material
+        self.color = color
+        self.isClean = isClean
+        self.hasFood = hasFood
+
+    def wash(self):
+        self.isClean = True
+
+    def dirty(self):
+        self.isClean = False
+
+    def eat(self):
+        self.hasFood = False
+        self.dirty()
+
+    def addFood(self):
+        self.hasFood = True
+
+    @staticmethod
+    def getInstance():
+        if Plate.instance is None:
+            Plate.instance = Plate(10, "ceramic", "blue")
+        return Plate.instance
+
+
+plate = Plate.getInstance()
+print(plate)
